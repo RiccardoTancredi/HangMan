4f2d45c11d931651e4ab52b11f6031eeb548623e
RiccardoTancredi <ric.tancredi@gmail.com>
2020-09-06 16:04:14 +0200

    Improvement in p5 library to draw  the hangman

--- hangman.py
+++ hangman.py
@@ -22,7 +22,7 @@ def initial_sostitution(original_parola:str, modified_parola:str, letter:str, in
         return continue_sostitution(original_parola, modified_parola, letter, index)       
     
 def life(i:int):
-    print("Hai ancora: ", 7-i,"vite")
+    print("Hai ancora: ", 6-i,"vite")
     return i
 
 def guess(letter:str, parola:str, modified_parola = ""):
@@ -46,20 +46,29 @@ def guess(letter:str, parola:str, modified_parola = ""):
             return modified_parola
 
 parola = input("Inserisci la parola senza farti vedere: ")
+parola = parola.lower()
 for i in range(11):
     print("*"*i)
 for i in range(9,0,-1):
     print("*"*i)
 j = 0
 h = word(parola)
-print(h)
 tentativo = ""
 finale = ""
+if " " in parola:
+    tentativo = guess(" ",parola, tentativo)
+    print(tentativo)
+else:
+    print(h)
 for i in range(len(parola)):
     finale += parola[i]
     finale += " "
-while j < 7:
+inserted_letters = []
+while j < 6:
     a = input("Inserisci una lettera:")
+    if a == "": 
+        a = input("Non hai inserito nulla, ritenta:")
+    a = a[0].lower()
     if tentativo != "":
         h = str(tentativo)
     if a in parola:
@@ -71,10 +80,16 @@ while j < 7:
         print(h)
         j +=1
         life(j)
+    if a in inserted_letters:
+        print("Hai già inserito questa lettere. Non ti distrarre, hai perso una vita")
+        print(tentativo)
+        j +=1
+        life(j)
+    inserted_letters.append(a)
     if tentativo == finale:
-        print("Hai indovito la parola,", finale ,"hai vinto")
+        print("Hai indovito la parola,", finale ,"\n HAI VINTO!!!")
         break
-    if j == 7:
+    if j == 6:
         print("Hai perso. La parola era: ", finale)
         break
 
